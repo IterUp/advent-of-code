1ae8edab4df5d0c4f2f3037946657e1a9ea44585
Paul Murphy <pamurphy@riotgames.com>
2023-12-03 09:42:36 +1100

    day1, part2 solution

--- day1/main.py
+++ day1/main.py
@@ -1,3 +1,20 @@
+translate = {
+        "one": "o1ne",
+        "two": "t2wo",
+        "three": "th3ree",
+        "four": "f4our",
+        "five": "f5ive",
+        "six": "s6ix",
+        "seven": "s7even",
+        "eight": "ei8ght",
+        "nine": "n9ine"}
+
+def convert(s):
+    s = s.strip()
+    for k, v in translate.items():
+        s = s.replace(k, str(v))
+    return s
+
 f = open("input.txt")
-lines = [[c for c in line if c.isdigit()] for line in f.readlines()]
+lines = [[c for c in convert(line) if c.isdigit()] for line in f.readlines()]
 print(sum(10*int(line[0])+int(line[-1]) for line in lines))
