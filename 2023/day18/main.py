019f78bf5f3d4a4e8ddcba1b2a0753fd118e07c0
Paul Murphy <paul.murph@gmail.com>
2023-12-19 14:22:04 +1100

    black formatting

--- 2023/day18/main.py
+++ 2023/day18/main.py
@@ -1,14 +1,16 @@
 directions = {
-    'U': (-1, 0),
-    'D': (1, 0),
-    'L': (0, -1),
-    'R': (0, 1),
+    "U": (-1, 0),
+    "D": (1, 0),
+    "L": (0, -1),
+    "R": (0, 1),
 }
 
 import time
 
+
 def approach_pos(start, end):
     return tuple(s + (1 if s < e else -1 if s > e else 0) for s, e in zip(start, end))
+
 
 class DigInstruction:
     def __init__(self, line):
@@ -17,7 +19,8 @@ class DigInstruction:
         self.distance = int(distance)
 
     def move(self, pos):
-        return tuple(p + self.distance*d for p, d in zip(pos, self.direction))
+        return tuple(p + self.distance * d for p, d in zip(pos, self.direction))
+
 
 class DigInstructions:
     def __init__(self, f):
@@ -27,15 +30,18 @@ class DigInstructions:
         self.fill()
         self.dump()
 
-    def get_verts(self, start_pos = (0, 0)):
+    def get_verts(self, start_pos=(0, 0)):
         pos = start_pos
         yield pos
         for instruction in self.instructions:
             pos = instruction.move(pos)
             yield pos
 
-    def get_bounds(self, start_pos = (0, 0)):
-        return [[f(coords) for coords in zip(*self.get_verts(start_pos))] for f in (min, max)]
+    def get_bounds(self, start_pos=(0, 0)):
+        return [
+            [f(coords) for coords in zip(*self.get_verts(start_pos))]
+            for f in (min, max)
+        ]
 
     def make_grid(self):
         bounds = self.get_bounds()
@@ -50,7 +56,7 @@ class DigInstructions:
                 # print(pos, next_pos)
                 draw_pos = pos
                 while draw_pos != next_pos:
-                    self.grid[draw_pos[1]][draw_pos[0]] = '#'
+                    self.grid[draw_pos[1]][draw_pos[0]] = "#"
                     draw_pos = approach_pos(draw_pos, next_pos)
                     # self.dump()
                     # print()
@@ -64,24 +70,26 @@ class DigInstructions:
                 is_inside = False
                 wall_count = 0
                 for col, c in enumerate(line):
-                    if c == '#':
+                    if c == "#":
                         wall_count += 1
                     else:
                         if wall_count == 1:
                             is_inside = not is_inside
                         elif wall_count > 1:
-                            is_inside = self.grid[row - 1][col] != '.'
+                            is_inside = self.grid[row - 1][col] != "."
                         wall_count = 0
                         if is_inside:
                             self.grid[row][col] = "*"
 
     def dump(self):
         for line in self.grid[:60]:
-            print(''.join(line[:100]))
+            print("".join(line[:100]))
 
     def count_lava(self):
-        return sum(line.count("#") for line in self.grid) + \
-            sum(line.count("*") for line in self.grid)
+        return sum(line.count("#") for line in self.grid) + sum(
+            line.count("*") for line in self.grid
+        )
+
 
 d = DigInstructions(open("input.txt"))
 print(d.count_lava())
