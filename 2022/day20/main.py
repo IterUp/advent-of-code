53668606a133905cb0b507375d5c7d0c67497a4f
Paul Murphy <paul.murph@gmail.com>
2023-12-23 08:53:59 +1100

    2022, Day 20, Part 1

--- 2022/day20/main.py
+++ 2022/day20/main.py
@@ -1,22 +1,21 @@
 numbers = [int(line) for line in open("input.txt")]
-for number in numbers:
-    count = numbers.count(number)
-    if count != 1:
-        print(count, number)
-    assert numbers.count(number) == 1
+new_indexes = list(range(len(numbers)))
 
-total = sum(numbers)
-new_numbers = numbers[:]
-for number in numbers:
-    index = new_numbers.index(number)
-    pre_mod = index + number
-    new_index = pre_mod%len(numbers) - (pre_mod < 0)
+for old_index, number in enumerate(numbers):
+    number = number % (len(numbers) - 1)
+    index = new_indexes.index(old_index)
+    new_index = (index + number) % (len(numbers) - 1)
     if index < new_index:
-        new_numbers[index:new_index] = new_numbers[index+1:new_index+1]
-        new_numbers[new_index] = number
+        new_indexes[index:new_index] = new_indexes[index + 1 : new_index + 1]
+        new_indexes[new_index] = old_index
     elif index > new_index:
-        new_numbers[new_index+1:index+1] = new_numbers[new_index:index]
-        new_numbers[new_index] = number
-    assert sum(new_numbers) == total
+        new_indexes[new_index + 1 : index + 1] = new_indexes[new_index:index]
+        new_indexes[new_index] = old_index
+new_numbers = [numbers[index] for index in new_indexes]
 index = new_numbers.index(0)
-print(sum(new_numbers[(index+offset)%len(new_numbers)] for offset in (1000, 2000, 3000)))
+print(
+    sum(
+        new_numbers[(index + offset) % len(new_numbers)]
+        for offset in (1000, 2000, 3000)
+    )
+)
