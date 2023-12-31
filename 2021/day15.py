13be66d5a8465e06edbd78c3418b047cc324d744
Paul Murphy <paul.murph@gmail.com>
2024-01-01 10:20:31 +1100

    2021, Day 15, Part 2

--- 2021/day15.py
+++ 2021/day15.py
@@ -17,7 +17,7 @@ def neighbours(grid, x, y):
         yield (x, y + 1)
 
 
-def part1(grid):
+def find_path_size(grid):
     heap = [(0, (0, 0))]
     end = (len(grid[0]) - 1, len(grid) - 1)
     visited = set()
@@ -30,12 +30,24 @@ def part1(grid):
             if neighbour not in visited:
                 visited.add(neighbour)
                 heappush(heap, (cost + grid[neighbour[1]][neighbour[0]], neighbour))
-    assert False
-    return 0
+    assert False, "No path found"
+
+
+def part1(grid):
+    return find_path_size(grid)
+
+
+def f(v):
+    return ((v - 1) % 9) + 1
 
 
 def part2(grid):
-    return 0
+    new_grid = [
+        [f(v + cell_x + cell_y) for cell_x in range(5) for v in row]
+        for cell_y in range(5)
+        for row in grid
+    ]
+    return find_path_size(new_grid)
 
 
 def main(input):
