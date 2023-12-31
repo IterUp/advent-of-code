ae05ca30379b84d518e643928e69c7259371ad2a
Paul Murphy <paul.murph@gmail.com>
2023-12-31 20:01:20 +1100

    2021, Day 7, Part 2

--- 2021/day7.py
+++ 2021/day7.py
@@ -7,8 +7,15 @@ def part1(positions):
     return sum(abs(p - mid) for p in positions)
 
 
+def cost(v):
+    return (v + 1) * v // 2
+
+
 def part2(positions):
-    return 0
+    start, end = min(positions), max(positions)
+    return min(
+        sum(cost(abs(p - mid)) for p in positions) for mid in range(start, end + 1)
+    )
 
 
 def main(filename):
