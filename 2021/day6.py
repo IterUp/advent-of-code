4792efe174bfa49977a3d5fc79d669e48ee474ad
Paul Murphy <paul.murph@gmail.com>
2023-12-31 19:37:54 +1100

    2021, Day 6, Part 2

--- 2021/day6.py
+++ 2021/day6.py
@@ -3,21 +3,29 @@ from collections import deque
 is_test = False
 
 
-def part1(fish_counts):
-    for day in range(80):
+def make_counts(fish):
+    return deque(fish.count(day) for day in range(9))
+
+
+def count_fish(fish, days):
+    fish_counts = make_counts(fish)
+    for day in range(days):
         fish_counts.rotate(-1)
         fish_counts[6] += fish_counts[-1]
     return sum(fish_counts)
 
 
+def part1(fish):
+    return count_fish(fish, 80)
+
+
 def part2(fish):
-    return 0
+    return count_fish(fish, 256)
 
 
 def main(filename):
     fish = [int(v) for v in open(filename).readline().split(",")]
-    fish_counts = deque(fish.count(day) for day in range(9))
-    print("Part 1:", part1(fish_counts))
+    print("Part 1:", part1(fish))
     print("Part 2:", part2(fish))
 
 
