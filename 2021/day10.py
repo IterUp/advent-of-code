caf2501743bfeb2dfcd951f0a2b1dc84d1614e7e
Paul Murphy <paul.murph@gmail.com>
2023-12-31 23:10:38 +1100

    2021, Day 10, Part 2

--- 2021/day10.py
+++ 2021/day10.py
@@ -4,6 +4,8 @@ matched = {")": "(", "]": "[", "}": "{", ">": "<"}
 
 scores = {")": 3, "]": 57, "}": 1197, ">": 25137}
 
+scores2 = {"(": 1, "[": 2, "{": 3, "<": 4}
+
 
 def syntax_check(line):
     stack = []
@@ -22,8 +24,27 @@ def part1(lines):
     return sum(syntax_check(line) for line in lines)
 
 
+def syntax_check2(line):
+    stack = []
+    for c in line:
+        if c in matched:
+            if stack and stack[-1] == matched[c]:
+                stack.pop()
+            else:
+                return 0
+        else:
+            stack.append(c)
+    return sum(scores2[c] * 5**i for i, c in enumerate(stack))
+
+
 def part2(lines):
-    return 0
+    scores = []
+    for line in lines:
+        score = syntax_check2(line)
+        if score:
+            scores.append(score)
+    scores.sort()
+    return scores[len(scores) // 2]
 
 
 def main(input):
