f3cba3d8c7328632eba27669dc8422766434b794
Paul Murphy <paul.murph@gmail.com>
2023-12-31 15:42:16 +1100

    2021, Day 2, Part 2

--- 2021/day2.py
+++ 2021/day2.py
@@ -15,4 +15,20 @@ def part1():
     return depth * pos
 
 
-print(part1())
+def part2():
+    aim = 0
+    depth = 0
+    pos = 0
+    for line in open(filename).read().splitlines():
+        command, size = line.split()
+        size = int(size)
+        if command == "forward":
+            pos += size
+            depth += aim * size
+        else:
+            aim += size if command == "down" else -size
+    return depth * pos
+
+
+print("Part 1:", part1())
+print("Part 2:", part2())
