ac6029aa7a1a097d87b93f56c4d7636f4b25a9aa
Paul Murphy <paul.murph@gmail.com>
2023-12-31 16:28:20 +1100

    2021, Day 3, Part 2

--- 2021/day3.py
+++ 2021/day3.py
@@ -10,9 +10,11 @@ def most_common(line):
 
 
 def least_common(line):
+    if len(line) == 1:
+        return line
     count0 = line.count("0")
     count1 = line.count("1")
-    return "0" if count0 < count1 else "1"
+    return "0" if count0 <= count1 else "1"
 
 
 def part1(lines):
@@ -22,7 +24,15 @@ def part1(lines):
 
 
 def part2(lines):
-    return 0
+    most = ""
+    least = ""
+    for i in range(len(lines[0])):
+        most += most_common("".join(line[i] for line in lines if line.startswith(most)))
+        least += least_common(
+            "".join(line[i] for line in lines if line.startswith(least))
+        )
+
+    return int(most, 2) * int(least, 2)
 
 
 print("Part 1:", part1(lines))
