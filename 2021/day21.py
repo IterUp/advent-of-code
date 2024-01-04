d8a548e3847c57ea3ccd5e5b1b44b3870768d57c
Paul Murphy <paul.murph@gmail.com>
2024-01-04 23:15:52 +1100

    2021, Day 21, Part 2

--- 2021/day21.py
+++ 2021/day21.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 is_test = False
 
 
@@ -5,7 +7,6 @@ def part1(p1, p2):
     num_rolls = 0
     p1score, p2score = 0, 0
     while p2score < 1000:
-        print(f"{p1=} {p2=} {p1score=} {p2score=} {num_rolls=}")
         for i in range(3):
             p1 += (num_rolls % 100) + 1
             num_rolls += 1
@@ -14,13 +15,47 @@ def part1(p1, p2):
         p1, p2 = p2, p1
         p1score, p2score = p2score, p1score
 
-    print(f"{p1score=} {p2score=}")
-
     return num_rolls * p1score
 
 
+dice_outcomes = defaultdict(int)
+for v in [a + b + c for a in range(1, 4) for b in range(1, 4) for c in range(1, 4)]:
+    dice_outcomes[v] += 1
+
+
+def num_wins(p1, p1score, p2, p2score, cache):
+    key = (p1, p1score, p2, p2score)
+    if key in cache:
+        return cache[key]
+    if p2score >= 21:
+        return (0, 1)
+
+    results = [
+        (
+            frequency,
+            num_wins(
+                p2,
+                p2score,
+                (p1 + dice_total - 1) % 10 + 1,
+                p1score + (p1 + dice_total - 1) % 10 + 1,
+                cache,
+            ),
+        )
+        for dice_total, frequency in dice_outcomes.items()
+    ]
+
+    result = (
+        sum(frequency * result[1] for frequency, result in results),
+        sum(frequency * result[0] for frequency, result in results),
+    )
+
+    cache[key] = result
+
+    return result
+
+
 def part2(p1, p2):
-    return 0
+    return max(num_wins(p1, 0, p2, 0, {}))
 
 
 def main(inputs):
