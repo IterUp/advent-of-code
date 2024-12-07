103e0be5d8d15da2c024e1dab34d326ed2904504
Paul Murphy <paul.murph@gmail.com>
2024-12-07 14:58:59 +1100

    Day 4. Part 1

--- 2024/day4/main.py
+++ 2024/day4/main.py
@@ -1,21 +1,22 @@
-def f(lines, col, row):
+def words(lines, col, row):
     deltas = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
     w = len(lines[0])
     h = len(lines)
     for dc, dr in deltas:
         c_end = col + dc * 3
-        r_end = col + dc * 3
+        r_end = row + dr * 3
         if (0 <= c_end < w) and (0 <= r_end < h):
-            yield "".join(lines[row + i * dc][col + i * dr])
+            yield "".join([lines[row + i * dr][col + i * dc] for i in range(4)])
 
 
 f = open("input.txt")
 lines = [line.strip() for line in f.readlines()]
+first = list(words(lines, 0, 0))
 print(
     sum(
         w == "XMAS"
-        for w in f(lines, c, r)
         for c in range(len(lines[0]))
         for r in range(len(lines))
+        for w in words(lines, c, r)
     )
 )
