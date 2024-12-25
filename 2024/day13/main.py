5a9d35c92c4aabad3f0da6dec0203c42a0d9f6fa
Paul Murphy <paul.murph@gmail.com>
2024-12-26 09:51:37 +1100

    Day 13 2024. Part 2.

--- 2024/day13/main.py
+++ 2024/day13/main.py
@@ -15,8 +15,9 @@ def convert(line1, line2, line3):
 games = [convert(*lines[4 * i : 4 * i + 3]) for i in range(len(lines) // 4 + 1)]
 
 
-def play_game(game):
+def play_game(game, offset):
     r1, r2, t = game
+    t = (t[0] + offset, t[1] + offset)
     b = (t[1] * r1[0] - t[0] * r1[1]) / (r2[1] * r1[0] - r2[0] * r1[1])
     a = (t[0] * r2[1] - t[1] * r2[0]) / (r1[0] * r2[1] - r1[1] * r2[0])
     result = (int(a), int(b))
@@ -25,22 +26,22 @@ def play_game(game):
     return None
 
 
-def part1(games):
+def solve(games, offset):
     total = 0
-    wins = 0
-    losses = 0
     for game in games:
-        tokens = play_game(game)
+        tokens = play_game(game, offset)
         if tokens:
             total += 3 * tokens[0] + tokens[1]
-            wins += 1
-        else:
-            losses += 1
     return total
 
 
+def part1(games):
+    return solve(games, 0)
+
+
 def part2(games):
-    return 0
+    offset = 10000000000000
+    return solve(games, offset)
 
 
 print("Part 1 =", part1(games))
