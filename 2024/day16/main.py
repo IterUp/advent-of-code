7989c80c646fb3a3796c698bc5e66694bf0ffcb3
Paul Murphy <paul.murph@gmail.com>
2025-09-18 17:44:09 +1000

    Day 16, Part 2

--- 2024/day16/main.py
+++ 2024/day16/main.py
@@ -1,3 +1,5 @@
+import heapq
+
 f = open("input.txt")
 
 grid = [line.strip() for line in f.readlines()]
@@ -13,37 +15,70 @@ def find_pos(grid, to_find):
 deltas = {0: (1, 0), 1: (0, 1), 2: (-1, 0), 3: (0, -1)}
 
 
-def part1(grid):
+def get_scores(grid):
     pos = find_pos(grid, "S")
-    end_pos = find_pos(grid, "E")
     d = 0
     scores = {}
-    stack = [(pos, d, False)]
+    stack = [(0, pos, d, False)]
     scores[(pos, d)] = 0
     while stack:
-        pos, d, was_turn = stack.pop()
+        score, pos, d, was_turn = heapq.heappop(stack)  # stack.pop()
+        # assert score == scores[(pos, d)]
         delta = deltas[d]
         next_pos = (pos[0] + delta[0], pos[1] + delta[1])
         if grid[next_pos[1]][next_pos[0]] != "#":
-            new_score = scores[(pos, d)] + 1
+            new_score = score + 1
             key = (next_pos, d)
-            if (key not in scores) or (new_score < scores[key]):
+            if key not in scores or new_score < scores[key]:
                 scores[key] = new_score
-                stack.append((next_pos, d, False))
+                heapq.heappush(
+                    stack, (new_score, next_pos, d, False)
+                )  # stack.append((next_pos, d, False))
         if not was_turn:
             for turn in (-1, 1):
-                new_score = scores[(pos, d)] + 1000
+                new_score = score + 1000
                 new_d = (d + turn) % 4
                 key = (pos, new_d)
-                if (key not in scores) or (new_score < scores[key]):
+                if key not in scores or new_score < scores[key]:
                     scores[key] = new_score
-                    stack.append((pos, new_d, True))
+                    heapq.heappush(
+                        stack, (new_score, pos, new_d, True)
+                    )  # stack.append((pos, new_d, True))
 
-    return min(scores[(end_pos, d)] for d in range(4) if (pos, d) in scores)
+    return scores
+
+
+def part1(grid):
+    scores = get_scores(grid)
+    end_pos = find_pos(grid, "E")
+    return min(scores[(end_pos, d)] for d in range(4) if (end_pos, d) in scores)
 
 
 def part2(grid):
-    return 0
+    scores = get_scores(grid)
+    end_pos = find_pos(grid, "E")
+    best_score = min(scores[(end_pos, d)] for d in range(4) if (end_pos, d) in scores)
+
+    visited = set()
+    positions = [
+        (end_pos, d) for d in range(4) if scores.get((end_pos, d)) == best_score
+    ]
+    while positions:
+        curr = positions.pop()
+        pos, d = curr
+        if curr not in visited:
+            visited.add(curr)
+            delta = deltas[d]
+            prev_pos = (pos[0] - delta[0], pos[1] - delta[1])
+            if scores[curr] - 1 == scores.get((prev_pos, d)):
+                positions.append((prev_pos, d))
+            for turn in (-1, 1):
+                new_d = (d + turn) % 4
+                if scores[curr] - 1000 == scores.get((pos, new_d)):
+                    positions.append((pos, new_d))
+
+    visited_without_direction = set([pos for pos, _ in visited])
+    return len(visited_without_direction)
 
 
 print("Part 1 =", part1(grid))
